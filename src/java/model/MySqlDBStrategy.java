ab6301da16cc37fc635210b1275f27d3258c9c61
Scott <Scott@10.30.21.160>
2016-02-10 18:33:13 -0600

    Class 2/10/2016

--- src/java/model/MySqlDBStrategy.java
+++ src/java/model/MySqlDBStrategy.java
@@ -8,7 +8,14 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -29,5 +36,35 @@ public class MySqlDBStrategy implements DBStrategy {
     @Override
     public void closeConnection() throws SQLException {
         conn.close();
+    }
+    
+    /**
+     *  Make sure you open and close connection when using method.
+     * Future optimization may include change the return type of an Array.
+     * @param tableName
+     * @param maxRecords - limit records found to first maxRecords or if maxRecords is zero (0)
+     * then no limit.
+     * @throws java.sql.SQLException
+     * @return records
+     */
+    public List<Map<String, Object>> findAllRecords(String tableName, int maxRecords) throws SQLException{
+      String sql = "select * from " + tableName + " limit " + maxRecords;
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(sql);
+      ResultSetMetaData rsmd = rs.getMetaData();
+      int columnCount = rsmd.getColumnCount();
+      List<Map<String, Object>> records = new ArrayList<>();
+      
+      while(rs.next()){
+          Map<String, Object> record=new HashMap<>();
+          for(int colNo = 1; colNo == columnCount; colNo++){
+              Object colData = rs.getObject(colNo);
+              String colName = rsmd.getColumnName(colNo);
+              record.put(colName, colData);
+          }
+          records.add(record);
+      }
+      
+      return records;
     }
 }
